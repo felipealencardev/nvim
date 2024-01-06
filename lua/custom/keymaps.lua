d726378d1a46c9a87a024c163e9836c095816909
Felipe Alencar <felipealencardev@gmail.com>
2024-01-06 18:26:39 -0300

    add more keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,26 +1,41 @@
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- delete single character without copying into register
-vim.keymap.set('n', 'x', '"_x')
+keymap.set('n', 'x', '"_x')
 
 -- window management
-vim.keymap.set("n", "<leader>sv", "<C-w>v")     -- split window vertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s")     -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=")     -- make split windows equal width & height
-vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' })
+keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' })
+keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make split windows equal width & height' })
+keymap.set('n', '<leader>sx', ':close<CR>', { desc = 'close current split window' })
 
 -- vim-maximizer
-vim.keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+keymap.set('n', '<leader>sm', ':MaximizerToggle<CR>')
 
 -- restart lsp server
-vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>")
+keymap.set('n', '<leader>rs', ':LspRestart<CR>')
+
+-- increment/decrement
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- tabs
+keymap.set('n', 'te', ':tabedit', opts)
+keymap.set('n', '<tab>', ':tabnext<Return>', opts)
+keymap.set('n', '<s-tab>', ':tabprev<Return>', opts)
